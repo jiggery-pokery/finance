1f02ca213b660db067cfafc4f0bde6ad3062b03a
jiggery-pokery <liew.audi+github@gmail.com>
2022-08-16 15:21:14 +0800

    Updates

---
+++ precache-manifest.8906991af014ee70ac3ad6bf1508e003.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a4b64c49395bd751bf7b42dc2f2260b",
+    "url": "/finance/index.html"
+  },
+  {
+    "revision": "057b4defaf1ed2896857",
+    "url": "/finance/static/css/4.4b306f74.chunk.css"
+  },
+  {
+    "revision": "465b1c526c715b61ee17",
+    "url": "/finance/static/css/5.a7812488.chunk.css"
+  },
+  {
+    "revision": "ee33b73170835d654cf6",
+    "url": "/finance/static/css/6.a7812488.chunk.css"
+  },
+  {
+    "revision": "dc9567eacd6de2f5fdda",
+    "url": "/finance/static/css/7.a7812488.chunk.css"
+  },
+  {
+    "revision": "48c9dde52240a0d9b99f",
+    "url": "/finance/static/css/main.68c4c948.chunk.css"
+  },
+  {
+    "revision": "0c9a50a74f3ed2c9fe0a",
+    "url": "/finance/static/js/0.7a2247e4.chunk.js"
+  },
+  {
+    "revision": "fec3848c885726419b28",
+    "url": "/finance/static/js/3.5d8ef893.chunk.js"
+  },
+  {
+    "revision": "057b4defaf1ed2896857",
+    "url": "/finance/static/js/4.f7228586.chunk.js"
+  },
+  {
+    "revision": "2f97bfe5b7c1c72b23be28739730a643",
+    "url": "/finance/static/js/4.f7228586.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "465b1c526c715b61ee17",
+    "url": "/finance/static/js/5.ac1e686b.chunk.js"
+  },
+  {
+    "revision": "ee33b73170835d654cf6",
+    "url": "/finance/static/js/6.a99363ae.chunk.js"
+  },
+  {
+    "revision": "dc9567eacd6de2f5fdda",
+    "url": "/finance/static/js/7.97227665.chunk.js"
+  },
+  {
+    "revision": "518a09bfb7829f0cff7b",
+    "url": "/finance/static/js/8.9a60c3e3.chunk.js"
+  },
+  {
+    "revision": "48c9dde52240a0d9b99f",
+    "url": "/finance/static/js/main.c389c11f.chunk.js"
+  },
+  {
+    "revision": "11480af882ac0e1c754d",
+    "url": "/finance/static/js/runtime-main.d1ffe30d.js"
+  }
+]);
